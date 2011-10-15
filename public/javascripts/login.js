498608d4c84e1209e4c9993161e684bb0d974ae7
dmuneras <dmunera119@gmail.com>
2011-10-15 12:34:57 -0500

    styles login and form user

---
+++ public/javascripts/login.js
@@ -0,0 +1,17 @@
+
+$('document').ready(function(){
+     
+     $("#login-div").expose({
+	
+	mask: {
+	    color: '#ebecff',
+	    loadSpeed: 200,
+	    opacity: 0.9
+	},
+	
+	closeOnClick: false
+	
+    });
+
+   
+});
