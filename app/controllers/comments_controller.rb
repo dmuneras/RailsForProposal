101c813d1684bcdcaf52fcb6589f49806bca741d
dmuneras <dmunera119@gmail.com>
2011-10-19 15:21:55 -0500

    add comments to request

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,55 @@
+class CommentsController < ApplicationController
+  def index
+    @commentable = find_commentable
+    @comments = @commentable.comments
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @commentable = find_commentable
+    @comment = @commentable.comments.build(params[:comment])
+    if @comment.save
+      flash[:notice] = "Successfully created comment."
+      redirect_to @commentable
+    else
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update_attributes(params[:comment])
+      redirect_to @comment, :notice  => "Successfully updated comment."
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to @comment.commentable
+  end
+
+  private
+
+  def find_commentable
+    params.each do |name, value|
+      if name =~ /(.+)_id$/
+        return $1.classify.constantize.find(value)
+      end
+    end
+    nil
+  end
+end
